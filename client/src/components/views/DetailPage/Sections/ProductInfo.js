c0c679362907a3e325752edac6cbc336cecdbd02
Kaylinkk <kyulim0429@163.com>
2022-03-14 13:28:43 +0900

    logo

--- client/src/components/views/DetailPage/Sections/ProductInfo.js
+++ client/src/components/views/DetailPage/Sections/ProductInfo.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Button, Descriptions, Rate, message } from 'antd';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../../../../_actions/user_actions';
+import '../Sections/ProductImage.css'
 
 function ProductInfo(props) {
     const dispatch = useDispatch();
@@ -15,7 +16,9 @@ function ProductInfo(props) {
 
     }
 
-    let website = `https://www.walmart.com/search?q=${props.detail.title}`
+    let walmart = `https://www.walmart.com/search?q=${props.detail.title}`
+    let safeway = `https://anycart.com/@sales/find/${props.detail.title}`
+    let amazonFresh = `https://www.amazon.com/s?k=${props.detail.title}`
 
 
     return (
@@ -38,29 +41,41 @@ function ProductInfo(props) {
                     <tr>
                         <td className="tg-c3ow">
 
-                            <a href={website}>
-                                <img src='https://cdn.corporate.walmart.com/dims4/WMT/cf80266/2147483647/strip/true/crop/855x305+0+0/resize/1960x700!/quality/90/?url=https%3A%2F%2Fcdn.corporate.walmart.com%2F98%2F28%2F342ccbff478ab025592645fafcfc%2Fwalmart-logo.png' alt="walmart"></img></a>
+                            <a href={walmart} target="_blank" rel="noopener noreferrer">
+                                <img className='img_logo' src='https://cdn.corporate.walmart.com/dims4/WMT/cf80266/2147483647/strip/true/crop/855x305+0+0/resize/1960x700!/quality/90/?url=https%3A%2F%2Fcdn.corporate.walmart.com%2F98%2F28%2F342ccbff478ab025592645fafcfc%2Fwalmart-logo.png' alt="walmart"></img></a>
                         </td>
                         <td className="tg-c3ow"></td>
                     </tr>
                     <tr>
-                        <td className="tg-c3ow"></td>
+                        <td className="tg-c3ow">
+                            <a href={safeway} target="_blank" rel="noopener noreferrer">
+                                <img className='img_logo' src='https://logotyp.us/files/safeway.svg' alt="safeway"></img></a>
+                        </td>
                         <td className="tg-c3ow"></td>
                     </tr>
                     <tr>
-                        <td className="tg-c3ow"></td>
+                        <td className="tg-c3ow">
+                            <a href={amazonFresh} target="_blank" rel="noopener noreferrer">
+                                <img className='img_logo' src='https://brandlogovector.com/wp-content/uploads/2021/09/Amazon-Fresh-Logo.png' alt="walmart"></img></a>
+                        </td>
                         <td className="tg-c3ow"> </td>
                     </tr>
                     <tr>
-                        <td className="tg-c3ow"></td>
-                        <td className="tg-c3ow"></td>
-                    </tr>
-                    <tr>
-                        <td className="tg-c3ow"></td>
+                        <td className="tg-c3ow">
+                            <a href={walmart} target="_blank" rel="noopener noreferrer">
+                                <img className='img_logo' src='https://cdn.corporate.walmart.com/dims4/WMT/cf80266/2147483647/strip/true/crop/855x305+0+0/resize/1960x700!/quality/90/?url=https%3A%2F%2Fcdn.corporate.walmart.com%2F98%2F28%2F342ccbff478ab025592645fafcfc%2Fwalmart-logo.png' alt="walmart"></img></a></td>
                         <td className="tg-c3ow"></td>
                     </tr>
                     <tr>
+                        <td className="tg-c3ow">
+                            <a href={walmart} target="_blank" rel="noopener noreferrer">
+                                <img className='img_logo' src='https://cdn.corporate.walmart.com/dims4/WMT/cf80266/2147483647/strip/true/crop/855x305+0+0/resize/1960x700!/quality/90/?url=https%3A%2F%2Fcdn.corporate.walmart.com%2F98%2F28%2F342ccbff478ab025592645fafcfc%2Fwalmart-logo.png' alt="walmart"></img></a>
+                        </td>
                         <td className="tg-c3ow"></td>
+                    </tr>
+                    <tr>
+                        <td className="tg-c3ow"><a href={walmart}>
+                            <img className='img_logo' src='https://cdn.corporate.walmart.com/dims4/WMT/cf80266/2147483647/strip/true/crop/855x305+0+0/resize/1960x700!/quality/90/?url=https%3A%2F%2Fcdn.corporate.walmart.com%2F98%2F28%2F342ccbff478ab025592645fafcfc%2Fwalmart-logo.png' alt="walmart"></img></a></td>
                         <td className="tg-c3ow"></td>
                     </tr>
                 </tbody>
